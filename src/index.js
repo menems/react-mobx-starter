e05e3996b1856768a18a54d78a83bffe2c660af2
blaz <blaz@menems.net>
2016-05-04 00:44:29 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Hello = () => <div>coucou</div>;
+
+ReactDom.render(<Hello />, document.getElementById('root'));
